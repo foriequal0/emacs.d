474fbfaea28ac9d20b08cbc6b6cdd1c753974622
SeongChan Lee <foriequal@gmail.com>
2015-03-12 02:13:37 +0900

    Load file using more consise method

--- init.el
+++ init.el
@@ -1,8 +1,9 @@
+(require 'cl)
 (require 'package)
 
 ;; bootstrap utils
 
 (setq custom-file "~/.emacs.d/custom.el")
-(if (file-readable-p custom-file) (load custom-file))
+(load custom-file 'noerror)
 
-(add-hook 'after-init-hook (lambda () (load "~/.emacs.d/init-real.el")))
+(load "~/.emacs.d/init-real.el")
