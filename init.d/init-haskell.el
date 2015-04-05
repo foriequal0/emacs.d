21ba78c9f9560c9b253f7d8a5228a4b4ae22bd22
SeongChan Lee <foriequal@gmail.com>
2015-04-06 03:33:41 +0900

    Add company-ghc for init-haskell

--- init.d/init-haskell.el
+++ init.d/init-haskell.el
@@ -19,5 +19,18 @@
   :require flycheck
   :init (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup))
 
+;; install cabal-install < 1.22 when ghc < 7.10
+;; tested with cabal-install == 1.20, ghc 7.8.2
+(req-package ghc
+  :require haskell-mode
+  :init (progn (autoload 'ghc-init "ghc" nil t)
+	       (autoload 'ghc-debug "ghc" nil t)
+	       (add-hook 'haskell-mode-hook (lambda () (ghc-init)))))
+
+(req-package company-ghc
+  :require (company ghc)
+  :init (progn (add-hook 'haskell-mode-hook 'company-mode)
+	       (add-to-list 'company-backends 'company-ghc)
+	       (custom-set-variables '(company-ghc-show-info t))))
 
 (provide 'init-haskell)
