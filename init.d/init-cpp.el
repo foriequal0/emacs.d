13992cd9a61237896784877147d2e6e644610704
SeongChan Lee <foriequal@gmail.com>
2015-04-06 02:38:29 +0900

    c++ mode and its relavant mode

---
+++ init.d/init-cpp.el
@@ -0,0 +1,28 @@
+(req-package irony
+  :require company
+  :init (progn (add-hook 'c++-mode-hook 'irony-mode)
+	       (add-hook 'c-mode-hook 'irony-mode)
+	       (add-hook 'objc-mode-hook 'irony-mode)
+	       
+	       (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+	       (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)))
+
+(defun my-irony-mode-hook ()
+  "Replace the `completion-at-point' and `complete-symbol' bindings in irony-mode's buffer by irony-mode's functino"
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+
+(req-package company-irony
+  :require company
+  :init (progn (eval-after-load 'company
+		 '(add-to-list 'company-backends 'company-irony))
+	       (add-hook 'irony-mode-hook 
+			 'company-irony-setup-begin-commands)))
+
+(req-package flycheck-irony
+  :require flycheck
+  :init (progn (add-hook 'flycheck-mode-hook #'flycheck-irony-setup)))
+
+(provide 'init-cpp)
