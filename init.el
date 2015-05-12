87f0787e26099b48b1353d546f2fccc785a36c58
SeongChan Lee <foriequal@gmail.com>
2015-05-12 15:38:57 +0900

    Move backup file setting to init.el & some fix.
    This changes ensures that backup strategy will applied to emacs even on
    failure of load init.d/* scripts.

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
 (require 'cl)
 (require 'package)
 
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup/"))
+      backup-by-copying t    ; Don't delink hardlinks
+      version-control t      ; Use version numbers on backups
+      delete-old-versions t  ; Automatically delete excess backups
+      kept-new-versions 6   ; how many of the newest versions to keep
+      kept-old-versions 2    ; and how many of the old
+      )
 
 (defun reload-dotemacs-file ()
   "reload your .emacs file without restarting Emacs"
