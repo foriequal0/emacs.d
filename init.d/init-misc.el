d74f4e1d839bb8c820da9be75472287cb46a93ef
SeongChan Lee <foriequal@gmail.com>
2015-05-20 10:43:48 +0900

    misc changes

--- init.d/init-misc.el
+++ init.d/init-misc.el
@@ -1,4 +1,8 @@
-;(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(add-hook 'after-make-frame-functions
+	  (lambda (frame)
+	    (when (not (display-graphic-p))
+	      (if (fboundp 'menu-bar-mode)
+		  (menu-bar-mode -1)))))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
@@ -12,9 +16,19 @@
 
 (display-battery-mode 1)
 
+(global-auto-revert-mode 1)
+
+(setq-default indent-tabs-mode t)
+(add-hook 'emacs-lisp-mode-hook indent-tabs-mode nil)
+(add-hook 'lisp-mode-hook indent-tabs-mode nil)
+
 (global-set-key (kbd "C-S-h") 'windmove-left)
 (global-set-key (kbd "C-S-l") 'windmove-right)
 (global-set-key (kbd "C-S-k") 'windmove-up)
 (global-set-key (kbd "C-S-j") 'windmove-down)
+
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(req-package iresize)
 
 (provide 'init-misc)
