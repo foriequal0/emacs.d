42b9cbe50b0170d400c89907de0dcf5fab51f22e
SeongChan Lee <foriequal@gmail.com>
2015-06-03 03:58:02 +0900

    Fix backup-directory-alist regex

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
 (require 'cl)
 (require 'package)
 
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup/"))
-      backup-by-copying t    ; Don't delink hardlinks
+(setq backup-directory-alist '((".*" . "~/.emacs.d/backup/")))
+(setq backup-by-copying t    ; Don't delink hardlinks
       version-control t      ; Use version numbers on backups
       delete-old-versions t  ; Automatically delete excess backups
       kept-new-versions 6   ; how many of the newest versions to keep
