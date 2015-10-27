9cd5f9545bba686ca433d04da96adbaf6d784c34
SeongChan Lee <foriequal@gmail.com>
2015-10-27 18:26:56 +0900

    Disable annoying checkdoc

--- pkg-init.d/init-flycheck.el
+++ pkg-init.d/init-flycheck.el
@@ -1,6 +1,7 @@
 (req-package flycheck
   :init (add-hook 'after-init-hook #'global-flycheck-mode)
   :config (progn
-            (diminish 'flycheck-mode)))
+            (diminish 'flycheck-mode)
+            (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))))
 
 (provide 'init-flycheck)
