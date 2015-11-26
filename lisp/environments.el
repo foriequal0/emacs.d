f13a9a08ca38ce30fc3f0d9c5678d4ba6aab5b29
Shawn Lee <foriequal@gmail.com>
2015-11-26 15:57:23 +0900

    create auto-save directory if not exist

--- lisp/environments.el
+++ lisp/environments.el
@@ -1,6 +1,11 @@
+(require 'f)
 
 (setq backup-directory-alist '((".*" . "~/.emacs.d/backup/")))
+
+(if (f-directory? "~/.emacs.d/auto-save/")
+    (f-mkdir "~/.emacs.d/auto-save"))
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save/" t)))
+
 (setq backup-by-copying t    ; Don't delink hardlinks
       version-control t      ; Use version numbers on backups
       delete-old-versions t  ; Automatically delete excess backups
