3500af56e855f78bd7ec4692252e7d47b74ef7f8
SeongChan Lee <foriequal@gmail.com>
2016-07-27 17:41:15 +0900

    Fix company and fci collision, limit quickhelp size, add helm-company

--- init.d/init-company-mode.el
+++ init.d/init-company-mode.el
@@ -7,8 +7,24 @@
   (global-company-mode)
   (setq company-tooltip-align-annotations t))
 
+;; hacky workaround for fci-mode
+(defun on-off-fci-before-company(command)
+  (when (string= "show" command)
+    (turn-off-fci-mode))
+  (when (string= "hide" command)
+    (turn-on-fci-mode)))
+
+(advice-add 'company-call-frontends :before #'on-off-fci-before-company)
+
 (use-package company-quickhelp
   :after company
-  :config (company-quickhelp-mode 1))
+  :config
+  (company-quickhelp-mode 1)
+  (setq company-quickhelp-max-lines 20))
+
+(use-package helm-company
+  :after company
+  :bind (:map company-active-map
+              ("M-h" . helm-company)))
 
 (provide 'init-company-mode)
