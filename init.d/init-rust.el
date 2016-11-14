0f1a78a3a8fde2ddf0781d2e4c3189e653beae9f
SeongChan Lee <foriequal@gmail.com>
2016-11-15 03:33:58 +0900

    rust-src-path from rust command.

--- init.d/init-rust.el
+++ init.d/init-rust.el
@@ -1,18 +1,24 @@
 (require 'use-package)
+(require 'dash)
 
 (use-package rust-mode
-  :mode "\\.rs\\'")
+  :mode "\\.rs\\'"
+  :config
+  (add-hook 'rust-mode-hook 'flycheck-rust-setup)
+  (add-hook 'rust-mode-hook 'racer-mode))
 
 (use-package flycheck-rust
-  :after rust-mode
-  :config (flycheck-rust-setup))
+  :defer t)
 
 (use-package racer
-  :after rust-mode
-  :init
-  (setq racer-rust-src-path "~/src/rustc/src")
-  (setq racer-cmd "~/bin/racer/target/release/racer")
+  :defer t
   :config
-  (racer-mode))
+  (-when-let* ((rustc (executable-find "rustc"))
+               (racer (executable-find "racer")))
+    (let* ((sysroot-raw (shell-command-to-string
+                         (concat rustc " --print sysroot")))
+           (sysroot (replace-regexp-in-string "\n\\'" "" sysroot-raw)))
+      (setq racer-rust-src-path (concat sysroot "/lib/rustlib/src/rust/src"))
+      (setq racer-cmd racer))))
 
 (provide 'init-rust)
