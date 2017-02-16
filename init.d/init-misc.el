eb0a5b2376f6dc18460b072a8f6810c044058ff3
SeongChan Lee <foriequal@gmail.com>
2017-02-17 03:29:15 +0900

    eager binding on avy

--- init.d/init-misc.el
+++ init.d/init-misc.el
@@ -213,12 +213,12 @@
     (add-hook 'dired-load-hook 'my-dired-init)))
 
 (use-package avy
-  :bind (("C-:" . avy-goto-char)
-         ("C-'" . avy-goto-char-2)
-         ("M-g f" . avy-goto-line)
-         ("M-g w" . avy-goto-word-1)
-         ("M-g e" . avy-goto-word-0))
   :config
+  (global-set-key (kbd "C-:") 'avy-goto-char)
+  (global-set-key (kbd "C-'") 'avy-goto-char-2)
+  (global-set-key (kbd "M-g f") 'avy-goto-line)
+  (global-set-key (kbd "M-g w") 'avy-goto-word-1)
+  (global-set-key (kbd "M-g e") 'avy-goto-word-0)
   (avy-setup-default))
 
 (use-package markdown-mode
